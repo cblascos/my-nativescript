36acf90f54b7e12c957bf3448f1ba6b906dfdb5f
Seba Wita <sebawita@gmail.com>
2016-01-26 23:06:11 +0000

    This is a starting point for the groceries app compatible with NativeScript 1.4.3

--- Groceries/app/app.js
+++ Groceries/app/app.js
@@ -1,8 +1,3 @@
-var application = require('application');
-
-application.mainModule = 'navigation/navigation';
-// START_CUSTOM_CODE_nativeScriptApp
-// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
-
-// END_CUSTOM_CODE_nativeScriptApp
-application.start();
+var applicationModule = require("application");
+applicationModule.mainModule = "views/login/login";
+applicationModule.start();
